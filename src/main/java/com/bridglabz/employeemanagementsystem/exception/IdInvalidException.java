3ba3bb5af06690854e58fc3fa2dd87ff75fe079a
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-08-14 11:56:06 +0530

    Revert "[Ariyan Pujari] added global exception handler"
    This reverts commit ec8695f442da1f6a53c3d8ad6f24ea401d23ecb0.

--- src/main/java/com/bridglabz/employeemanagementsystem/exception/IdInvalidException.java
+++
@@ -1,7 +0,0 @@
-package com.bridglabz.employeemanagementsystem.exception;
-
-public class IdInvalidException extends Exception{
-    public IdInvalidException(String message){
-        super(message);
-    }
-}
